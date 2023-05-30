0db2af28b9df1f0730f7a712a03add7e09ecb064
4naro <131971606+4naro@users.noreply.github.com>
2023-05-30 19:07:11 +0200

    esercizio finito

--- pg/m1/esercizi.sql
+++ pg/m1/esercizi.sql
@@ -24,12 +24,12 @@ from employee
 where salary > 10000;
 
 -- con salario minore di 4000 o maggiore di 15000
--- select first_name, last_name, phone, hired, salary
--- from employee
--- where salary not between 4000 AND salary 15000;
+select first_name, last_name, phone, hired, salary
+from employee
+where (salary < 4000 or salary > 15000);
 
 -- con salario minore di 4000 o maggiore di 15000, ma solo per i dipartimenti 5 e 8
--- select first_name, last_name, phone, hired
--- from employee
--- where department_id in (5, 8) 
+select first_name, last_name, phone, hired, salary, department_id
+from employee
+where (salary < 4000 or salary > 15000) AND department_id in (5, 8);
 
